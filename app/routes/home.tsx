13971ef7f70dbd5a9229e7700ebedfbc47d3b0d3
Johney Sun <ibelieveme383@gmail.com>
2025-09-10 00:38:17 +0900

    Test: SSG Implemented

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,19 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
+import type { LoaderFunctionArgs } from "react-router";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  return { message: "Hello from loader" };
+}
+
 
 export default function Home() {
   return <Welcome />;
