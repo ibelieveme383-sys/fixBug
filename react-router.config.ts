f9be179e6034470e817340552d187263f54c1cbd
Johney Sun <ibelieveme383@gmail.com>
2025-09-10 20:26:18 +0900

    Test: SSR: True

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "@react-router/dev/config";
+import { ssr } from "virtual:react-router/server-build";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
   prerender: async () => {
     // Return an array of paths to prerender, e.g. ["/", "/about"]
     return ["/"];
